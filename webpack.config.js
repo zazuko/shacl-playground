afbb3b8b18bc024500e8f50eeba51ccb89b9b9f9
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-04-15 10:22:59 +0200

    chore: init repo

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { createDefaultConfig } = require("@open-wc/building-webpack");
+
+module.exports = merge(
+  createDefaultConfig({
+    input: path.resolve(__dirname, "./src/index.html")
+  }),
+  {
+    resolve: {
+      extensions: [".mjs", ".js", ".json"],
+      alias: {
+        stream: "readable-stream"
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.nq$/,
+          use: ["raw-loader"]
+        },
+        {
+          test: /\.ttl$/,
+          use: ["raw-loader"]
+        }
+      ]
+    },
+    node: {
+      crypto: true
+    }
+  }
+);
