47f1dbe415fbfd624f8656fda59a6140dd519389
tpluscode <tpluscode@users.noreply.github.com>
2021-04-16 15:48:20 +0200

    style: lint setup and fix violations

--- src/lib/components/editor-drawer.js
+++ src/lib/components/editor-drawer.js
@@ -1,10 +1,10 @@
-import {connect} from '@captaincodeman/rdx';
-import {html, LitElement} from 'lit-element';
-import '@vaadin/vaadin-form-layout/vaadin-form-layout.js'
-import '@vaadin/vaadin-select/vaadin-select.js'
-import { formats } from '@rdfjs-elements/formats-pretty'
-import {store} from '../store/index.js';
-import './prefix-list.js'
+import { connect } from "@captaincodeman/rdx";
+import { html, LitElement } from "lit-element";
+import "@vaadin/vaadin-form-layout/vaadin-form-layout.js";
+import "@vaadin/vaadin-select/vaadin-select.js";
+import { formats } from "@rdfjs-elements/formats-pretty";
+import { store } from "../store/index.js";
+import "./prefix-list.js";
 
 class EditorDrawer extends connect(store, LitElement) {
   static get properties() {
@@ -12,45 +12,55 @@ class EditorDrawer extends connect(store, LitElement) {
       format: { type: String },
       model: { type: String },
       prefixes: { type: Array }
-    }
+    };
   }
 
   constructor() {
-    super()
-    this.prefixes = []
+    super();
+    this.prefixes = [];
   }
 
   render() {
-    return html`<vaadin-form-layout>
-      <vaadin-select label="Format" value="${this.format}" @value-changed="${this.__formatSelected}">
-        <template>
-          <vaadin-list-box>
-            ${Object.values(formats).map((format) => html`
-              <vaadin-item value="${format}">${format}</vaadin-item>
-            `)}
-          </vaadin-list-box>
-        </template>
-      </vaadin-select>
-    `
+    return html`
+      <vaadin-form-layout>
+        <vaadin-select
+          label="Format"
+          value="${this.format}"
+          @value-changed="${this.__formatSelected}"
+        >
+          <template>
+            <vaadin-list-box>
+              ${Object.values(formats).map(
+                format => html`
+                  <vaadin-item value="${format}">${format}</vaadin-item>
+                `
+              )}
+            </vaadin-list-box>
+          </template>
+        </vaadin-select>
+      </vaadin-form-layout>
+    `;
   }
 
   mapState(state) {
     return {
       format: state[this.model].format,
-      prefixes: state[this.model].prefixes,
-    }
+      prefixes: state[this.model].prefixes
+    };
   }
 
   mapEvents() {
     return {
-      'prefix-selected': e => store.dispatch[this.model].addPrefix(e.detail.value),
-      'prefix-unselected': e => store.dispatch[this.model].removePrefix(e.detail.value)
-    }
+      "prefix-selected": e =>
+        store.dispatch[this.model].addPrefix(e.detail.value),
+      "prefix-unselected": e =>
+        store.dispatch[this.model].removePrefix(e.detail.value)
+    };
   }
 
   __formatSelected(e) {
-    store.dispatch[this.model].changeFormat(e.detail.value)
+    store.dispatch[this.model].changeFormat(e.detail.value);
   }
 }
 
-customElements.define('editor-drawer', EditorDrawer)
+customElements.define("editor-drawer", EditorDrawer);
--- src/lib/components/error-summary.js
+++ src/lib/components/error-summary.js
@@ -1,88 +1,106 @@
-import {html, LitElement} from 'lit-element'
-import { shrink } from '@zazuko/rdf-vocabularies/shrink'
-import TermMap from '@rdf-esm/term-map'
-import {fromPointer} from '@rdfine/shacl/lib/ValidationResult';
-import {sh} from '@tpluscode/rdf-ns-builders';
+import { html, LitElement } from "lit-element";
+import { shrink } from "@zazuko/rdf-vocabularies/shrink";
+import TermMap from "@rdf-esm/term-map";
+import { fromPointer } from "@rdfine/shacl/lib/ValidationResult";
+import { sh } from "@tpluscode/rdf-ns-builders";
 
 function createMessage(result) {
   if (result.resultMessage) {
-    return result.resultMessage
+    return result.resultMessage;
   }
 
   if (result.sourceConstraintComponent) {
-    return `Violated ${shrink(result.sourceConstraintComponent.id.value)}`
+    return `Violated ${shrink(result.sourceConstraintComponent.id.value)}`;
   }
 
-  return 'Unspecified error'
+  return "Unspecified error";
 }
 
-const renderResult = (result) => html`<li>${createMessage(result)}</li>`
+const renderResult = result =>
+  html`
+    <li>${createMessage(result)}</li>
+  `;
 
-function renderSummary({ errors, focusNodes }) {
-  return html`<ul>
-    ${errors.map(renderResult)}
-    ${[...focusNodes].map(([focusNode, { properties, errors }]) => html`<li>
-      ${focusNode.value}:
-      <ul>
-        ${errors.map(renderResult)}
-        ${[...properties].map(([property, messages]) => html`<li>
-          ${shrink(property.value)}:
-          <ul>
-            ${messages.map(renderResult)}
-          </ul>
-        </li>`)}
-      </ul>
-    </li>`)}
-  </ul>`
+function renderSummary({ focusNodes, ...top }) {
+  return html`
+    <ul>
+      ${top.errors.map(renderResult)}
+      ${[...focusNodes].map(
+        ([focusNode, { properties, errors }]) => html`
+          <li>
+            ${focusNode.value}:
+            <ul>
+              ${errors.map(renderResult)}
+              ${[...properties].map(
+                ([property, messages]) => html`
+                  <li>
+                    ${shrink(property.value)}:
+                    <ul>
+                      ${messages.map(renderResult)}
+                    </ul>
+                  </li>
+                `
+              )}
+            </ul>
+          </li>
+        `
+      )}
+    </ul>
+  `;
 }
 
 function reduceToFocusNodes({ focusNodes, errors }, result) {
   if (result.focusNode) {
-    const focusNodeErrors = focusNodes.get(result.focusNode) || { properties: new TermMap(), errors: [] }
+    const focusNodeErrors = focusNodes.get(result.focusNode) || {
+      properties: new TermMap(),
+      errors: []
+    };
     if (result.resultPath) {
-      const pathErrors = focusNodeErrors.properties.get(result.resultPath.id)
+      const pathErrors = focusNodeErrors.properties.get(result.resultPath.id);
       if (pathErrors) {
-        pathErrors.push(result)
+        pathErrors.push(result);
       } else {
-        focusNodeErrors.properties.set(result.resultPath.id, [result])
+        focusNodeErrors.properties.set(result.resultPath.id, [result]);
       }
     } else {
-      focusNodeErrors.errors.push(result)
+      focusNodeErrors.errors.push(result);
     }
 
-    focusNodes.set(result.focusNode, focusNodeErrors)
+    focusNodes.set(result.focusNode, focusNodeErrors);
   } else {
-    errors.push(result)
+    errors.push(result);
   }
 
-  return { focusNodes, errors }
+  return { focusNodes, errors };
 }
 
-class ErrorSummary extends  LitElement {
+class ErrorSummary extends LitElement {
   static get properties() {
     return {
       validationResults: { type: Array }
-    }
+    };
   }
 
   constructor() {
-    super()
-    this.validationResults = []
+    super();
+    this.validationResults = [];
   }
-
 
   render() {
     const results = this.validationResults
       .map(result => fromPointer(result))
-      .filter(result => result.resultSeverity.equals(sh.Violation))
+      .filter(result => result.resultSeverity.equals(sh.Violation));
 
     if (results.length) {
-      const summary = results.reduce(reduceToFocusNodes, { focusNodes: new TermMap(), errors: [] })
-      return renderSummary(summary)
+      const summary = results.reduce(reduceToFocusNodes, {
+        focusNodes: new TermMap(),
+        errors: []
+      });
+      return renderSummary(summary);
     }
 
-    return ''
+    return "";
   }
 }
 
-customElements.define('error-summary', ErrorSummary)
+customElements.define("error-summary", ErrorSummary);
