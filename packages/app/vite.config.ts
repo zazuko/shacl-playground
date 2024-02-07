6395975e216d144d1b367017984eb05cc3ff1f8e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-02-07 09:47:05 +0100

    Remove yarn (#75)
    * build: remove yarn
    
    * fix: missing changelog
    
    * ci: use npm
    
    * ci(netlify): build command
    
    * docs: update readme
    
    * revert: netlify build

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
           dest: '.'
         },
         {
-          src: '../../CHANGELOG.md',
+          src: './CHANGELOG.md',
           dest: '.'
         }
       ]
